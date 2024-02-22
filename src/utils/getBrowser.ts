e554a5323b162600231f33c0f2cbe0e906a77af2
Wanderson Barradas <wandersonbarradas07@gmail.com>
2024-02-22 17:26:31 -0300

    - adicionando dependencias

--- src/utils/getBrowser.ts
+++ src/utils/getBrowser.ts
@@ -1,22 +1,31 @@
 import chromium from "chrome-aws-lambda";
-import puppeteer from "puppeteer-core";
-import path from "path";
+import puppeteer_core from "puppeteer-core";
+import puppeteerLib from "puppeteer";
 
 export const getBrowser = async () => {
-    const browser = puppeteer.launch({
-        executablePath:
-            process.env.NODE_ENV !== "development"
-                ? await chromium.executablePath
-                : path.join(
-                      "C:\\",
-                      "Program Files",
-                      "Google",
-                      "Chrome",
-                      "Application",
-                      "chrome.exe",
-                  ),
-        headless:
-            process.env.NODE_ENV !== "development" ? chromium.headless : true,
-    });
+    let chrome = {};
+    let puppeteer;
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        chrome = chromium;
+        puppeteer = puppeteer_core;
+    } else {
+        puppeteer = puppeteerLib;
+    }
+    let options = {};
+
+    if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+        options = {
+            args: [
+                ...chromium.args,
+                "--hide-scrollbars",
+                "--disable-web-security",
+            ],
+            defaultViewport: chromium.defaultViewport,
+            executablePath: await chromium.executablePath,
+            headless: true,
+            ignoreHTTPSErrors: true,
+        };
+    }
+    const browser = puppeteer.launch(options);
     return browser;
 };
